df4502a11118eb8561406ad9db51d345bcd9a8b6
Vinijs <vini-spider-6@hotmail.com>
2024-07-19 18:52:52 -0300

    Desafio 21 dias - AULA 07 - 19/07/2024

---
+++ array.rb
@@ -0,0 +1,19 @@
+a = [1,2,3]
+b = ['Danilo', 'Rosi', 'Woto']
+c = [true, false, true]
+
+puts b[2]
+
+a << 1
+
+a.push(1)
+
+a.pop()
+
+['Danilo', 'Rosi', 'Woto'].each do |value|
+  puts value
+end
+
+ss = {}
+
+data_encerramento = Time.now + 1.minuto
--- exec-aula-06.rb
+++ exec-aula-06.rb
@@ -6,21 +6,39 @@
 notas = []
 pesquisa = true
 
-while pesquisa do
+data_encerramento = Time.now + 60
+
+puts data_encerramento
+
+puts Time.now
+
+while Time.now < data_encerramento do
   puts "========================================================================"
   puts "   Pesquisa de satisfação Tornese um programador"
   puts "========================================================================"
   puts "De uma escala de 0 a 10 qual sua nota em relação ao conteúdo da Comunidade?"
   nota = gets.to_f
 
+  #puts "A pesquisa vaiser encerrada as: #{data_encerramento.strftime("%d/%m/%Y %H:%M")}"
+  if data_encerramento < Time.now
+    puts "A pesquisa irá se encerrar em: #{Time.at((data_encerramento - Time.now)).strftime("%M:%S")}"
+  else
+    puts "A pesquisa se encerrou"
+  end
+
+
+
+  #break if nota == -1
+
   if(nota > 10)
     puts "Nota inválida"
-    return
+    next
   end
 
-  break if nota == -1
+  
 
   notas << nota
+  
   end
 
-  puts "O seu NPS foi de : #{(notas.sum / notas.length)}"
+    puts "O seu NPS foi de : #{(notas.sum / notas.length)}"
