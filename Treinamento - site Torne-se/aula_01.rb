1e8669f69079a3e65689c46364e4dabae15ae193
Vinijs <vini-spider-6@hotmail.com>
2024-07-24 01:18:37 -0300

    Desafio 21 dias - AULA 10 - 23/07/2024

---
+++ Treinamento - site Torne-se/aula_01.rb
@@ -0,0 +1,7 @@
+10.times{|i| puts i}
+
+a = 1
+
+b = 2
+
+put a + b
--- funcoes.rb
+++ funcoes.rb
@@ -7,52 +7,47 @@
 # Faça o programa em app console utilizando 
 # variáveis, loop, condicionais e array
 
-system 'clear'
-require 'byebug'
 
-puts "=================BEM-VINDO A ESCOLA DE MÚSICA ROBERTO CARLOS ============="
-
-puts "=========================================================================="
-
-i = 0
-
-alunos = []
-
-while i != 4 do
-
-  puts "Para começar, selecione uma opção do menu: "
+def menu
+    puts "Para começar, selecione uma opção do menu: "
     puts "1 - CADASTRAR ALUNO"
     puts "2 - CADASTRAR NOTA DOS ALUNOS"
     puts "3 - RELATÓRIO DAS NOTAS E MÉDIA DOS ALUNOS"
     puts "4 - SAIR"
-    
-    i = gets.to_i
+end
 
-  case i
-    when 1
+def nenhum_aluno_cadastrado
+  puts "Atenção! Nenhum aluno cadastrado!"
+end
+
+def cadastrar_aluno
       aluno = {matricula: "",nome: "",notas: []}
       puts "Digite a matricula do aluno: "
       aluno[:matricula] = gets.to_s
       puts "Digite o nome do aluno: "
       aluno[:nome] = gets.to_s
       alunos.push(aluno)
-    when 2
-      puts "Informe a matricula do aluno: "
-      matricula = gets
-      aluno_encontrado = false
-      alunos.each do |a|
-        if a[:matricula] == matricula
-          puts "Informe a nota do aluno: "
-          a[:notas] << gets.to_f
-          aluno_encontrado = true
-          break
-        end
+end
+
+def cadastrar_nota_aluno
+    puts "Informe a matricula do aluno: "
+    matricula = gets
+    aluno_encontrado = false
+    alunos.each do |a|
+      if a[:matricula] == matricula
+        puts "Informe a nota do aluno: "
+        a[:notas] << gets.to_f
+        aluno_encontrado = true
+        break
       end
-      puts "Aluno não encontrado" unless aluno_encontrado
-      when 3
+    end
+    puts "Aluno não encontrado" unless aluno_encontrado
+end
+
+def relatorio
         puts "RELATÓRIO DE MÉDIAS E NOTAS DOS ALUNOS"
         if alunos.length == 0
-          puts "Atenção! Nenhum aluno cadastrado!"
+          nenhum_aluno_cadastrado
         else
           alunos.each do |aluno|
             puts "============================================================="
@@ -62,6 +57,33 @@ while i != 4 do
             # puts aluno
           end
         end 
+end
+
+system 'clear'
+require 'byebug'
+
+puts "=================BEM-VINDO A ESCOLA DE MÚSICA ROBERTO CARLOS ============="
+
+puts "=========================================================================="
+
+menu()
+puts "........................................................................"
+menu
+puts "........................................................................"
+
+i = 0
+
+alunos = []
+
+while i != 4 do
+    i = gets.to_i
+  case i
+    when 1
+      cadastrar_aluno
+    when 2
+      cadastrar_nota_aluno
+      when 3
+      relatorio
       when 4
         break;
     end
