d13e81ee2164a697c14511d14211f9cb8a3df0c3
Vinijs <vini-spider-6@hotmail.com>
2024-08-09 18:44:25 -0300

    Aula 15 - ORIENTAÇÃO A OBJETOS - POLIMORFISMO E OVERLOAD

---
+++ Treinamento - site Torne-se/aula_15.rb
@@ -0,0 +1,16 @@
+require 'byebug'
+require_relative 'Carro'
+require_relative 'Fiesta'
+require_relative 'Golf'
+
+
+
+fiesta = Fiesta.new
+golf = Golf.new
+carro = Carro.new
+
+golf.andar
+
+debugger
+
+Carro.new.mostrar
