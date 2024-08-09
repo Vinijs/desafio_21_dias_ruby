097a735f5dc00d9bbd9282bfd848fb3f83d0f3d7
Vinijs <vini-spider-6@hotmail.com>
2024-08-09 12:18:31 -0300

    Aula 14 - ORIENTAÇÃO A OBJETOS - PUBLIC, PRIVATE, PROTECTED

--- Treinamento - site Torne-se/Carro.rb
+++ Treinamento - site Torne-se/Carro.rb
@@ -8,6 +8,38 @@ class Carro
   attr_reader :pneu
 
   def mostrar(marca = "Marca Padrão")
-    puts "Marca: #{marca} - Modelo: #{self.nome}"
+    # puts "Marca: #{marca} - Modelo: #{self.nome} - #{algo_mais}"
+    puts "Marca: #{marca} - Modelo: #{self.nome} - #{algo_mais_protegido}"
   end
+
+  private
+
+  def algo_mais
+    "este é um metodo privado para retornar algo a mais"
+  end
+
+  def algo_mais2
+    "este é um metodo privado para retornar algo a mais"
+  end
+
+  def algo_mais3
+    "este é um metodo privado para retornar algo a mais"
+  end
+
+  def algo_mais4
+    "este é um metodo privado para retornar algo a mais"
+  end
+
+  public
+
+  def algo_mais_publico
+    "este é um metodo publico para retornar algo a mais"
+  end
+
+  protected
+
+  def algo_mais_protegido
+   "este é um metodo protegido para retornar algo a mais"
+  end
+
 end
--- Treinamento - site Torne-se/Fiesta.rb
+++ Treinamento - site Torne-se/Fiesta.rb
@@ -1,3 +1,13 @@
 class Fiesta < Carro
   attr_accessor :cor
+
+  def mostrar_privado
+    algo_mais
+    Carro.new.algo_mais
+  end
+
+  def mostrar_protegido
+    # algo_mais_protegido
+    Carro.new.algo_mais_protegido
+  end
 end
