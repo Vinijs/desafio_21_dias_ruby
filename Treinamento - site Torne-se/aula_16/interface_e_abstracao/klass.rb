c5564dc038807544ed4fc63816cd997477b946a8
Vinijs <vini-spider-6@hotmail.com>
2024-08-13 00:06:48 -0300

    Aula 16 - ORIENTAÇÃO A OBJETOS - INTERFACE, ABSTRAÇÃO, SINGLETON

---
+++ Treinamento - site Torne-se/aula_16/interface_e_abstracao/klass.rb
@@ -0,0 +1,12 @@
+class Klass < Abstrata
+  def initialize
+  end
+
+  # def test1
+  #   puts "implementado test 1"      
+  # end
+
+  # def test2
+  #   puts "implementado test 2"      
+  # end
+end
